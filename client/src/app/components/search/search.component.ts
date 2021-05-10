ca25640a4e4a65e7fcd1e2a10845800308f60fac
Sean Dillon <seandillon59@gmail.com>
2021-05-10 12:06:05 +0200

    feat(search): added functionality to search component
    Search now updates variables and renders options, when options are clicked they are transfered to a
    list that is rendered and are removed from the selectable options. Styles are added to selected
    options to denote that they are selected

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -15,6 +15,8 @@ export class SearchComponent implements OnInit {
   allOptions: TaggedItem[] = [];
   
   filteredOptions: TaggedItem[] = [];
+  @Input()
+  selectedOptions: TaggedItem[] = [];
   
   constructor() { }
 
@@ -22,11 +24,25 @@ export class SearchComponent implements OnInit {
   }
 
   search(query: string): void {
-      this.searchString = query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+      this.searchString = query.toLowerCase()
       const regex = new RegExp(`^${query}`,'i')
-      this.filteredOptions = query.length ? this.allOptions.filter( (option) => 
-        option.tagName.match(regex) ? true : false
+      this.filteredOptions = query.length ? this.allOptions.filter( (option) =>  
+        option.tagName.toLowerCase().includes(this.searchString, 0)
+        && !this.selectedOptions.includes(option)
       ) : [];
+   
+  }
+
+  selectOption(option: TaggedItem): void {
+    this.selectedOptions.push(option);
+    const index = this.filteredOptions.indexOf(option)
+    this.filteredOptions.splice(index, 1)
+  }
+
+  unSelectOption(option: TaggedItem): void {
+    this.filteredOptions.push(option)
+    const index = this.selectedOptions.indexOf(option)
+    this.selectedOptions.splice(index, 1)
   }
 
 
