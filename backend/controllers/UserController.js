0ea8e21f222ca0e6066f80becbd0419c36c7fc2f
JohnAL95 <joao.afonso.1995@gmail.com>
2021-05-10 22:19:21 +0200

    feat(user-controller): updated user controller to return complete data

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,26 +7,31 @@ const getUsers = async (req, res) => {
     let users = await db.User.findAll({
       include: [
         {
+          model: db.Country,
+          as: 'country'
+        },
+        {
+          model: db.ExperienceLevel,
+          as: 'experience_level'
+        },
+        {
+          model: db.DeveloperType,
+          as: 'developer_type'
+        },
+        {
           model: db.Technology,
-          attributes: ["id"],
           through: 'UserTechnologies',
           as: 'technologies'
         },
         {
           model: db.Country,
-          attributes: ["id"],
           through: 'UserEligibleCountries',
           as: 'eligible_countries'
         }
       ]
     });
 
-    users = users.map((user) => {
-      user = user.toJSON();
-      const technologies = user.technologies.map((tech) => tech.id);
-      const eligible_countries = user.eligible_countries.map((country) => country.id);
-      return {...user, technologies, eligible_countries};
-    });
+    users = users.map(user => user.toJSON());
 
     res.status(200).json(users);
   }
