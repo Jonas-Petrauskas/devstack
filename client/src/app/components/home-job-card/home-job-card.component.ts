79bfc2ffe14e2da1dbdd8f2c09e79f9affbf35c6
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-05-10 12:58:02 +0200

    feat(home): added card component

---
+++ client/src/app/components/home-job-card/home-job-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-job-card',
+  templateUrl: './home-job-card.component.html',
+  styleUrls: ['./home-job-card.component.scss']
+})
+export class HomeJobCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
