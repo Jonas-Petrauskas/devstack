30cb70e80628adb8f1fe2d607196f2c59400e65e
JohnAL95 <joao.afonso.1995@gmail.com>
2021-05-11 12:02:55 +0200

    feat(user-controller): added endpoint to query for filtered users

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,8 @@
 
 const db = require('../models/_index');
 
+const processUserQueryParams = require('../functions/user-query-params-getter');
+
 const getUsers = async (req, res) => {
   try {
     let users = await db.User.findAll({
@@ -40,8 +42,68 @@ const getUsers = async (req, res) => {
   }
 }
 
-const postUser = async (req, res) => {
+const getFilteredUsers = async (req, res) => {
   try {
+    const { query } = req.params;
+    const queries = processUserQueryParams(query);
+
+    const where = {}
+
+    if (queries.experience_level) where.experience_level_id = queries.experience_level;
+    if (queries.developer_type) where.developer_type_id = queries.developer_type;
+    if (queries.technologies) where['$technologies.id$'] = queries.technologies;
+
+    let users = await db.User.findAll({
+      where: where,
+      include: [
+        {
+          model: db.Country,
+          as: 'country'
+        },
+        {
+          model: db.ExperienceLevel,
+          as: 'experience_level'
+        },
+        {
+          model: db.DeveloperType,
+          as: 'developer_type'
+        },
+        {
+          model: db.Technology,
+          through: 'UserTechnologies',
+          as: 'technologies'
+        },
+        {
+          model: db.Country,
+          through: 'UserEligibleCountries',
+          as: 'eligible_countries'
+        }
+      ]
+    });
+
+    // TODO: FIX THIS TO BE DONE ON THE QUERY ABOVE
+    if (queries.technologies) {
+      users = users.filter((user) => {
+        const userTechIds = user.technologies.map(tech => tech.dataValues.id);
+
+        for (let i = 0; i < queries.technologies.length; ++i) {
+          if (!userTechIds.includes(queries.technologies[i])) return false;
+        }
+
+        return true;
+      });
+    }
+    // TODO ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    res.status(200).json(users);
+  }
+  catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+const postUser = async (req, res) => {
+  try { // TODO !
     const users = await db.User.findAll();
     res.status(201).json(users);
   }
@@ -52,5 +114,6 @@ const postUser = async (req, res) => {
 
 module.exports = {
   getUsers,
+  getFilteredUsers,
   postUser,
 }
--- backend/routes/private-router.js
+++ backend/routes/private-router.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const userController = require('../controllers/UserController');
 
 router.get('/users', userController.getUsers);
+router.get('/users/:query', userController.getFilteredUsers);
 
 module.exports = router;
