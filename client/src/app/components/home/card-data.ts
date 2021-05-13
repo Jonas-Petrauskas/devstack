4db9683e4660b6c604bc02c9fdd71b668a72ab20
JohnAL95 <joao.afonso.1995@gmail.com>
2021-05-13 15:27:42 +0200

    feat(home-component): refactored home component

--- client/src/app/components/home/card-data.ts
+++
@@ -1,7 +0,0 @@
-export const cardData = [
-  {
-    "name": "Jonas Petrauskas",
-    "title": "Junior Frotend Developer",
-    "description": "We’re looking for a Full Stack Software Engineer to join the team at Google, a leading Tech company with offices in San Francisco and Barcelona"
-  }
-]
