6970a2b0352c6456fea977fafb7e466c169ef1b3
JohnAL95 <joao.afonso.1995@gmail.com>
2021-05-18 17:52:53 +0200

    feat(login-structure): added logins for developers and companies

--- backend/routes/public-router.js
+++ backend/routes/public-router.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 
 const companyController = require('../controllers/CompanyController');
 const countryController = require('../controllers/CountryController');
+const developerController = require('../controllers/DeveloperController');
 const developerTypeController = require('../controllers/DeveloperTypeController');
 const technologyController = require('../controllers/TechnologyController');
 const experienceLevelController = require('../controllers/ExperienceLevelController');
 
 // Public routes, accessible without an authorisation token.
-router.get('/login/company', companyController.loginCompany);
-// router.get('/login/developer', developerTypeController.loginDeveloper); // TODO
+router.post('/login/company', companyController.loginCompany);
+router.post('/login/developer', developerController.loginDeveloper);
 
 router.get('/countries', countryController.getCountries);
 router.get('/developer_types', developerTypeController.getDeveloperTypes);
