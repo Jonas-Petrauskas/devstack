83319236537764c052a4d76e85cc0ee0abec1d04
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-05-19 15:01:13 +0200

    feat(fix): added new information into home cards and fixed styling

--- client/src/app/components/home/cards.ts
+++ client/src/app/components/home/cards.ts
@@ -9,13 +9,13 @@ export const card1 = {
 export const card2 = {
   "name": "Mo",
   "title": "Junior Full Stack Developer",
-  "description": "We’re looking for a Full Stack Software Engineer to join the team at Google, a leading Tech company with offices in San Francisco and Barcelona",
+  "description": "Are you a front end developer with a strong practical knowledge of JavaScript? We might have the perfect role for you.",
   "photoPath": "https://ca.slack-edge.com/T0WU5R8NT-UU3FJ9XA4-84f401a73723-512"
 }
 
 export const card3 = {
   "name": "Sara",
   "title": "Junior Frontend Developer",
-  "description": "We’re looking for a Full Stack Software Engineer to join the team at Google, a leading Tech company with offices in San Francisco and Barcelona",
+  "description": "The stack currently being used in most services consists of Linux, Nginx, MySQL and Python. For the front-end: Vue.JS, Vuetify and some legacy Django templates",
   "photoPath": "https://ca.slack-edge.com/T0WU5R8NT-U012D657JRM-19b45e42efc6-512"
 }
